78c925231699141297ad34153851c0ce7650db20
Evgeny <evgeny1bogdanov@gmail.com>
2023-03-02 22:13:31 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,43 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?? 'Viola\'s nail salon';?></title>
+    <link rel="icon" type="image/x-icon" href="./img/nail-polish-64.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    
+</head>
+<body>
+    <header id="header" class="fixed-top">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Viola's Nail Salon</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Galleria</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Hinnasto</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Ajanvaraus</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Tietoa meista</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Yhteystiedot</a>
+                </li>               
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </header>
+    
+    
+
