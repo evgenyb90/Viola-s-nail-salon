c49e631fbcbd6a57cc4d4b73b1e042f080022424
Evgeny <evgeny1bogdanov@gmail.com>
2023-03-04 19:45:54 +0200

    login

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <header id="header" class="fixed-top">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">Viola's Nail Salon</a>
+            <a class="navbar-brand" href="./index.php">Viola's Nail Salon</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -32,7 +32,10 @@
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" href="#">Yhteystiedot</a>
-                </li>               
+                </li> 
+                <li class="nav-item">
+                <a class="nav-link" href="./login/kirjautuminen.php">Kirjautuminen</a>
+                </li>              
             </ul>
             </div>
         </div>
